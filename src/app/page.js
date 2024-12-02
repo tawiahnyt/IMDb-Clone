37366233205d7dede6bb87097b75eaa84b509466
tawiah <tawiahin4k@gmail.com>
2024-12-02 15:06:57 +0000

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
+/** @format */
+import Results from "@/components/Results";
+import dotenv from "dotenv";
+dotenv.config();
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = (await searchParams).genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+  // console.log(results);
+
   return (
-    <h1 className="text-red-400">Hello</h1>
+    <div>
+      <Results results={results} />
+    </div>
   );
 }
