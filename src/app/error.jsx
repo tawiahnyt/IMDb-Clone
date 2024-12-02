056325306420cb62266a2d8bc143013f316926e0
tawiah <tawiahin4k@gmail.com>
2024-12-02 16:17:12 +0000

    error handling

---
+++ src/app/error.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+"use client";
+
+import React, { useEffect } from "react";
+
+export default function error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10">
+      <h1>Something went wrong</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
