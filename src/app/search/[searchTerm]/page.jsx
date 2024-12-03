418a8a11f4cc43b4408ca55a2d1a2fd67446471b
tawiah <tawiahin4k@gmail.com>
2024-12-03 12:29:04 +0000

    done

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
 import dotenv from "dotenv";
-import ErrorMessage from '@/app/error';
-import Results from '@/components/Results';
+import ErrorMessage from "@/app/error";
+import Results from "@/components/Results";
 
 dotenv.config();
 const API_KEY = process.env.API_KEY;
 
 export default async function SearchPage({ params }) {
-    console.log(params);
-    
-    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${params.searchTerm}&language=en-US&adult=false`)
+  // console.log(params);
 
-    if (!res.ok){
-        throw new ErrorMessage("Error fetching data")
-    }
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${params.searchTerm}&language=en-US&adult=false`
+  );
 
-    const data = await res.json()
-    const results = data.results
+  if (!res.ok) {
+    throw new ErrorMessage("Error fetching data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
   return (
     <div>
-    {results && results.length == 0 && (
-        <h1 className='text-center pt-6'>No results found</h1>
-    )}
+      {results && results.length == 0 && (
+        <h1 className="text-center pt-6">No results found</h1>
+      )}
 
-    {results && <Results results={results} />}
+      {results && <Results results={results} />}
     </div>
-  )
+  );
 }
